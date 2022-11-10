defbbba4a9de8ce13f211675436337f5c43fc10f
Joey Kang <joeykang96@gmail.com>
2022-11-10 10:18:53 -0800

    clean up react template, attempt to create react component for TopSongs

--- songsearch/src/App.js
+++ songsearch/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="SongSearch">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        // Insert code here
       </header>
     </div>
   );
 }
+
+function TopSongs() {
+  return (
+    <div>
+      <h3> Top Songs of the day </h3>
+
+      <ol class="list-group list-group-flush list-group-numbered">
+        <li class="list-group-item bg-transparent">Infiite Ammo - Le Castle Vania</li>
+        <li class="list-group-item bg-transparent">Dance Monkey - Tones and I</li>
+        <li class="list-group-item bg-transparent">Blinding Lights - The Weekend</li>
+        <li class="list-group-item bg-transparent">I Don't Care - Ed Sheeran</li>
+        <li class="list-group-item bg-transparent">Mother - Charlie Puth</li>
+        <li class="list-group-item bg-transparent">Higher Lover - Kygo</li>
+      </ol>
+    </div>
+  );
+}
+
+const topSongsElement = <TopSongs />
+
+let topSongsRoot = ReactDOM.createRoot(document.getElementById('topsongs'));
+root.render(topSongsElement);
 
 export default App;
