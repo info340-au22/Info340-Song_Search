b53c1fa84408f8da7f439f0951437f2140c4a51a
yinanericxue <102645083+yinanericxue@users.noreply.github.com>
2022-12-12 19:12:33 -0800

    updated error

--- songsearch/src/components/Upload.js
+++ songsearch/src/components/Upload.js
@@ -5,15 +5,15 @@ export function Upload(props) {
         <main>
             <div className="ms-5">
                 <form>
-                  <label for="songName" style="color:white" style="font-size:300%;">Song Name:</label>
-                  <input type="text" id="songName" name="songName"><br><br>
-                  <label for="artist" style="color:white" style="font-size:300%;">Artist:</label>
-                  <input type="text" id="artist" name="artist"><br><br>
-                  <label for="genre" style="color:white" style="font-size:300%;">Genre:</label>
-                  <input type="text" id="genre" name="genre"><br><br>
-                  <label for="link" style="color:white" style="font-size:300%;">Spotify Link:</label>
-                  <input type="text" id="link" name="link"><br><br>
-                  <input type="submit">
+                  <label for="songName" style="color:white font-size:300%;">Song Name:</label>
+                  <input type="text" id="songName" name="songName"></input><br></br>
+                  <label for="artist" style="color:white font-size:300%;">Artist:</label>
+                  <input type="text" id="artist" name="artist"></input><br></br>
+                  <label for="genre" style="color:white font-size:300%;">Genre:</label>
+                  <input type="text" id="genre" name="genre"></input><br></br>
+                  <label for="link" style="color:white font-size:300%;">Spotify Link:</label>
+                  <input type="text" id="link" name="link"></input><br></br>
+                  <input type="submit"></input>
                 </form>
             </div>
         </main>
